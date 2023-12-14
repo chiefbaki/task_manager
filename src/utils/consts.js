e3c2d1edab1a8598a61678b5dfde73a7d95ae406
IslamKurbanov <ik509331@gmail.com>
2023-12-14 19:55:31 +0600

    fix api

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,4 +1,7 @@
-export const API = 'http://localhost:8000/TODOS'
+// local api
+//export const API = 'http://localhost:8000/TODOS'
 
+// remote api
+export const API = 'https://vercel-api-pi-nine.vercel.app/TODOS'
 
 export let ADMINS = ["admin@admin.com"]
